a6f7030612920fb1ea0c9e7bb115d5a4d19bf8d2
Kasem32833 <abulkasem32833@gmail.com>
2026-02-22 13:50:56 +0300

    create a separate js file and link with html

---
+++ 1.5 Connect JS file/main.js
@@ -0,0 +1,7 @@
+
+        window.alert("Take Care Your Eyes");
+        
+            document.write("This js connet withing script tag from different file");
+            document.getElementById("heading").innerHTML = "This is JS learning";
+            console.log("This is a console function");
+    
