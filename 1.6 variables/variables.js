e9064cb5b263d1a06f65ff0760e5cdaba1f43073
Md Abul Kasem <abulkasem32833@gmail.com>
2026-02-22 22:34:39 +0300

    variable with var and let keyword and rules of variable declaration

---
+++ 1.6 variables/variables.js
@@ -0,0 +1,50 @@
+var x ;
+//price = 10;
+
+// var age = 39;
+
+//here = is an assignment operator;
+
+
+/*
+ can't start with number
+ can't start with keyword / reserve keyword like (let, const, var, for, while)
+ can't keep space between two word
+
+
+ can start with a-z or A-Z
+ can start with $
+ can start with _ (Underscore)
+ can use underscore 
+ can write camel case-> myName, productImage
+
+
+  name, Name, $name, _name
+ variable name case sensitive
+
+
+
+
+*/
+
+var userName = "Abul";
+
+var userName = "Kasem";
+// var use করে sameName আবার ব্যাবহার করা যাবে
+// use let insted of var
+
+let productName ;
+
+
+
+// let ব্যাবহার করলে পুনরায় declare করা যাবেনা কিন্তু value assign করা যাবে
+
+
+let bestProduct = "honor-x9d"
+
+
+
+
+document.write(bestProduct);
+
+console.log(bestProduct);
