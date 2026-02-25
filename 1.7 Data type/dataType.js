8e0f87649daab16b72962760a2811a73b97415ce
Md Abul Kasem <abulkasem32833@gmail.com>
2026-02-25 11:03:16 +0300

    Primitive and non primitive data types

---
+++ 1.7 Data type/dataType.js
@@ -0,0 +1,37 @@
+/*
+    Primitive Data Type (Immutable -- ভ্যালু পরিবর্তন করা যায়না )
+
+        1. Number -- Integer, Floating Point
+            let price = 23 or 23.5;
+        2. String -- Text
+            let userName = "Abul"
+        3. Boolean -- True or False
+            let isStudent = true or false
+        4. Null -- Empty Value
+            let amount = null;
+        5. Undefined -- Value Not Assigned
+            let address;
+        6. Symbol -- Unique Identifier
+            let uniqueId = symbol("id")
+        7. BigInt -- Vary Large Integer
+            let bigInteger = 123456789012345677890n
+
+    Non-Primitive Data Type (Mutable -- ভ্যালু পরিবর্তন করা যায় )
+
+        1. Object -- 
+        2. Array
+        3. Function(object)
+
+
+ */
+
+    let product = {
+        productName: "Mobile",
+        price: 20000,
+    }
+
+    let fruits = ["Mango", "Banana", "Lichi", ]
+
+
+    //array কে array() দিয়ে ও প্রকাশ করা যায়
+
