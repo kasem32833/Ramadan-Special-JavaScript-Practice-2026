c849264a9c1eb1b9562dc505f3e80fc77142a58a
Kasem32833 <abulkasem32833@gmail.com>
2026-03-02 13:36:37 +0300

    javaScript string methods

---
+++ 2.3 String Method/stringMethod.js
@@ -0,0 +1,25 @@
+let js = "javaScript is a Programming Language";
+
+//console.log("js.length");
+
+// get charcter using index number
+//console.log(js[14]);
+
+//get charcter using method
+//console.log(js.charAt(16));
+
+//split string
+console.log(js.substr(4, 6 ));
+
+//split string with substring output will show before last number
+console.log(js.substring(4, 10));
+
+//সবগুলো বড় হাতের লিখতে
+console.log(js.toUpperCase());
+
+//সবগুলো ছোট হাতের লিখতে
+console.log(js.toLowerCase());
+
+
+//সবগুলো ছোট হাতের লিখতে
+console.log(js.toLowerCase());
